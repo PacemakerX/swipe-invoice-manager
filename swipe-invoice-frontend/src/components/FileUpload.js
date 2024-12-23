bf9827a8c3b99ff34cbcb8205127229ee699178b
PacemakerX <sparsh.officialwork@gmail.com>
2024-12-23 10:48:44 +0530

    updates the url for rendered on vercel

--- swipe-invoice-frontend/src/components/FileUpload.js
+++ swipe-invoice-frontend/src/components/FileUpload.js
@@ -34,7 +34,7 @@ const FileUpload = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3001/api/upload",
+        "https://swipe-invoice-manager.onrender.com/api/upload",
         formData,
         {
           headers: {
